6d0cd1041b2a26fa3b3b94a3bbba04cdeede732d
Jake <Jake3231@users.noreply.github.com>
2023-11-04 14:47:02 -0500

    Initial Commit

---
+++ Home-Evaluator/Home_EvaluatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_EvaluatorApp.swift
+//  Home-Evaluator
+//
+//  Created by Jake Spann on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Home_EvaluatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
