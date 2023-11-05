6d9e55bbff7cd4f4c85fbd599ae1ab3a1218c5eb
Jason Antwi-Appah <hey@jasonaa.me>
2023-11-05 03:40:08 -0600

    Add basic estimation model

--- Home-Evaluator/ReportView.swift
+++ Home-Evaluator/ReportView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 import RoomPlan
 import SceneKit
+import CoreML
 
 struct ReportView: View {
     @Binding var capturedRooms: [CapturedRoom]
     @State var areaInSqFt: [String: Float] = [:]
     @State var scenes: [String:URL] = [:]
     var address: String = ""
+	@State var valueEstimate: Double = -1.0
+	@State var model: MyTabularRegressor_1?
+	@State var input: MyTabularRegressor_1Input? = MyTabularRegressor_1Input(PROPERTY_TYPE: "Single Family Residential", CITY: "Richardson", ZIP_OR_POSTAL_CODE: 78641.0, BEDS: 4.0, BATHS: 2.5, SQUARE_FEET: 2294.0, LOT_SIZE: 3000.0, YEAR_BUILT: 2018.0, __SQUARE_FEET: 169.14, HOA_MONTH: 30.0, LATITUDE: 30.634521, LONGITUDE: -97.849760)
     
     var body: some View {
         /*List(capturedRooms.flatMap({$0.sections}) as! [CapturedRoom.Section], id: \.label) { section in
@@ -36,6 +40,33 @@ struct ReportView: View {
             //Text(section.label.rawValue)
         }*/
         
+
+		Text("Estimate: \(valueEstimate, specifier: "%.2f")").task {
+			if model == nil {
+				let config = MLModelConfiguration()
+				config.computeUnits = .all
+				
+				do {
+					model = try MyTabularRegressor_1(configuration: config)
+				} catch {
+					print("Model instantiation failed.", error)
+				}
+			}
+			
+			guard let model = model else {
+				return
+			}
+		
+			if let input = input {
+				do {
+					let output = try model.prediction(input: input)
+					valueEstimate = output.PRICE
+				} catch {
+					print("Prediction error occured.", error)
+				}
+			}
+		}
+
         List(capturedRooms, id: \.identifier) { room in
             Section(content: {
                 ForEach(room.sections, id: \.label) { section in
